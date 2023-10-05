de8bf609deea31eb7bea2933a5ac2c9ab9bdd061
Renara <rehxavier100@hotmail.com>
2023-10-04 22:08:26 -0300

    Condições Encadeadas

---
+++ Estrutudas-de-Repetições/src/CondicoesEncadeadas.java
@@ -0,0 +1,19 @@
+public class CondicoesEncadeadas {
+
+ public static void main(String[] args) {
+  
+  int nota= 10;
+
+  if (nota >= 7 )
+  System.out.println("Aprovado");
+  
+    
+    else if (nota < 6 )
+    System.out.println("Recuperação");
+
+  else 
+  System.out.println("Reprovado");
+    
+  }
+ }
+
